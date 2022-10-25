84e32566b4fcf5eebebb45ec81f5a051f8fb35d2
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2022-10-25 23:04:38 +0300

    удалил лишний класс, добавил Arrays.toString

--- lesson 7 one-dimensional-array/netology/src/Main.java
+++ lesson 7 one-dimensional-array/netology/src/Main.java
@@ -31,6 +31,7 @@ public class Main {
                 }
             }
             System.out.println("Средняя температура: " + (sum / cnt));
+            System.out.println(Arrays.toString(temps));
         }
     }
 }
