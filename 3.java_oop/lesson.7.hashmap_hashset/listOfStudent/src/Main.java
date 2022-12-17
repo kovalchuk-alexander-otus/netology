4271b5afbfdc88751631d47fbeccf3a789b5f312
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2022-12-17 14:56:04 +0300

    main: вернул настройки стиля по-умолчанию ... и отформатировал в соответствии с ними код

--- 3.java_oop/lesson.7.hashmap_hashset/listOfStudent/src/Main.java
+++ 3.java_oop/lesson.7.hashmap_hashset/listOfStudent/src/Main.java
@@ -13,22 +13,24 @@ public class Main {
 
         // диалог с пользователем
         while (true) {
-            System.out.println("Введите информацию о студенте: \"ФИО" +
-                    ", номер группы" +
-                    ", номер студенческого билета\"\n" +
-                    "(для завершения работы программы введите " +
-                    "\"end\")\n > ");
+            System.out.println("Введите информацию о студенте: \"ФИО, номер группы, номер студенческого билета\"\n" +
+                    "(для завершения работы программы введите \"end\")\n > ");
             input = scanner.nextLine();
             if (input.equals("end")) break;
 
             record = input.split(",");
             if (record.length != 3) {
-                System.out.println("Не корректно введена запись - заполняйте в " +
-                        "соответствии с шаблоном!");
+                System.out.println("Не корректно введена запись - заполняйте в соответствии с шаблоном!");
                 continue;
             }
-            students.add(new Student(record[0].trim(), record[1].trim(),
-                    record[2].trim()));
+
+            Student student = new Student(record[0].trim(), record[1].trim(), record[2].trim());
+            if (students.contains(student)) {
+                System.out.printf("Ошибка! Студент с номером студенческого билета [%s] уже сохранен в системе.%n",
+                        record[2].trim());
+            } else {
+                students.add(student);
+            }
         }
 
         // выводим на экран
