4a591d774a29752f57dd5a18b8ede251211ddae9
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-01-05 00:06:52 +0300

    main: домашка со 1.java-basics 2.algoritms 3.java_oop README.md script.sh test по TreeSet/TreeMap форматирование

--- 3.java_oop/lesson.8.treemap_treeset/candidate/src/Main.java
+++ 3.java_oop/lesson.8.treemap_treeset/candidate/src/Main.java
@@ -22,30 +22,18 @@ public class Main {
                 new TreeSet<>(Comparator.comparing(Candidate::getRelevance, Comparator.reverseOrder())
                         .thenComparing(Candidate::getRating, Comparator.reverseOrder()).thenComparing(Candidate::getFullName));
 
-        candidates.add(new Candidate("Ковальчук", "Александр", "Михайлович", Sex.MALE, 43
-                , 5, 5));
-        candidates.add(new Candidate("Петров", "Владислав", "Иванович", Sex.MALE, 34
-                , 1, 5));
-        candidates.add(new Candidate("Бабкин", "Илья", "Олегович", Sex.MALE, 56
-                , 4, 5));
-        candidates.add(new Candidate("Ряполов", "Олег", "Игоревич", Sex.MALE, 23
-                , 3, 5));
-        candidates.add(new Candidate("Андреев", "Семен", "Моисеевич", Sex.MALE, 41
-                , 2, 5));
-        candidates.add(new Candidate("Губанов", "Вячеслав", "Сергеевич", Sex.MALE, 39
-                , 5, 4));
-        candidates.add(new Candidate("Мармеладов", "Леонид", "Артемович", Sex.MALE, 39
-                , 5, 5));
-        candidates.add(new Candidate("Газманов", "Иннокентий", "Павлович", Sex.MALE, 39
-                , 5, 4));
-        candidates.add(new Candidate("Глазунов", "Илья", "Афанасьевич", Sex.MALE, 39
-                , 4, 4));
-        candidates.add(new Candidate("Петропавлов", "Никанор", "Гуторович", Sex.MALE, 39
-                , 4, 2));
-        candidates.add(new Candidate("Герцен", "Литий", "Петрович", Sex.MALE, 39
-                , 4, 5));
-        candidates.add(new Candidate("Муромский", "Игнатий", "Львович", Sex.MALE, 39
-                , 5, 2));
+        candidates.add(new Candidate("Ковальчук", "Александр", "Михайлович", Sex.MALE, 43, 5, 5));
+        candidates.add(new Candidate("Петров", "Владислав", "Иванович", Sex.MALE, 34, 1, 5));
+        candidates.add(new Candidate("Бабкин", "Илья", "Олегович", Sex.MALE, 56, 4, 5));
+        candidates.add(new Candidate("Ряполов", "Олег", "Игоревич", Sex.MALE, 23, 3, 5));
+        candidates.add(new Candidate("Андреев", "Семен", "Моисеевич", Sex.MALE, 41, 2, 5));
+        candidates.add(new Candidate("Губанов", "Вячеслав", "Сергеевич", Sex.MALE, 39, 5, 4));
+        candidates.add(new Candidate("Мармеладов", "Леонид", "Артемович", Sex.MALE, 39, 5, 5));
+        candidates.add(new Candidate("Газманов", "Иннокентий", "Павлович", Sex.MALE, 39, 5, 4));
+        candidates.add(new Candidate("Глазунов", "Илья", "Афанасьевич", Sex.MALE, 39, 4, 4));
+        candidates.add(new Candidate("Петропавлов", "Никанор", "Гуторович", Sex.MALE, 39, 4, 2));
+        candidates.add(new Candidate("Герцен", "Литий", "Петрович", Sex.MALE, 39, 4, 5));
+        candidates.add(new Candidate("Муромский", "Игнатий", "Львович", Sex.MALE, 39, 5, 2));
 
         System.out.println("\n\nTreeSet - отсортировали по релевантности и рейтингу\n");
         System.out.println(candidates);
