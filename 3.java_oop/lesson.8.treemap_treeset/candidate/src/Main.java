3e565da5d125ac96526b2c5bd08c8a4d017290e6
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-01-05 00:04:12 +0300

    main: домашка со 1.java-basics 2.algoritms 3.java_oop README.md script.sh test по TreeSet/TreeMap форматирование

--- 3.java_oop/lesson.8.treemap_treeset/candidate/src/Main.java
+++ 3.java_oop/lesson.8.treemap_treeset/candidate/src/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // вариант с TreeSet
-        Set <Candidate> candidates = trySet();
+        Set<Candidate> candidates = trySet();
 
         // вариант с List
         tryList((TreeSet<Candidate>) candidates);
@@ -16,35 +16,35 @@ public class Main {
     // вариант с TreeSet
     //   ... т.к. строит уникальность на базе Comparator - то пришлось добавить уникальное поле в сортировку
     //   ... иначе, могли теряться записи
-    public static Set<Candidate> trySet(){
+    public static Set<Candidate> trySet() {
 
         Set<Candidate> candidates =
                 new TreeSet<>(Comparator.comparing(Candidate::getRelevance, Comparator.reverseOrder())
                         .thenComparing(Candidate::getRating, Comparator.reverseOrder()).thenComparing(Candidate::getFullName));
 
-        candidates.add(new Candidate("Ковальчук", "Александр","Михайлович", Sex.MALE, 43
+        candidates.add(new Candidate("Ковальчук", "Александр", "Михайлович", Sex.MALE, 43
                 , 5, 5));
-        candidates.add(new Candidate("Петров", "Владислав","Иванович", Sex.MALE, 34
+        candidates.add(new Candidate("Петров", "Владислав", "Иванович", Sex.MALE, 34
                 , 1, 5));
-        candidates.add(new Candidate("Бабкин", "Илья","Олегович", Sex.MALE, 56
+        candidates.add(new Candidate("Бабкин", "Илья", "Олегович", Sex.MALE, 56
                 , 4, 5));
-        candidates.add(new Candidate("Ряполов", "Олег","Игоревич", Sex.MALE, 23
+        candidates.add(new Candidate("Ряполов", "Олег", "Игоревич", Sex.MALE, 23
                 , 3, 5));
-        candidates.add(new Candidate("Андреев", "Семен","Моисеевич", Sex.MALE, 41
+        candidates.add(new Candidate("Андреев", "Семен", "Моисеевич", Sex.MALE, 41
                 , 2, 5));
-        candidates.add(new Candidate("Губанов", "Вячеслав","Сергеевич", Sex.MALE, 39
+        candidates.add(new Candidate("Губанов", "Вячеслав", "Сергеевич", Sex.MALE, 39
                 , 5, 4));
-        candidates.add(new Candidate("Мармеладов", "Леонид","Артемович", Sex.MALE, 39
+        candidates.add(new Candidate("Мармеладов", "Леонид", "Артемович", Sex.MALE, 39
                 , 5, 5));
-        candidates.add(new Candidate("Газманов", "Иннокентий","Павлович", Sex.MALE, 39
+        candidates.add(new Candidate("Газманов", "Иннокентий", "Павлович", Sex.MALE, 39
                 , 5, 4));
-        candidates.add(new Candidate("Глазунов", "Илья","Афанасьевич", Sex.MALE, 39
+        candidates.add(new Candidate("Глазунов", "Илья", "Афанасьевич", Sex.MALE, 39
                 , 4, 4));
-        candidates.add(new Candidate("Петропавлов", "Никанор","Гуторович", Sex.MALE, 39
+        candidates.add(new Candidate("Петропавлов", "Никанор", "Гуторович", Sex.MALE, 39
                 , 4, 2));
-        candidates.add(new Candidate("Герцен", "Литий","Петрович", Sex.MALE, 39
+        candidates.add(new Candidate("Герцен", "Литий", "Петрович", Sex.MALE, 39
                 , 4, 5));
-        candidates.add(new Candidate("Муромский", "Игнатий","Львович", Sex.MALE, 39
+        candidates.add(new Candidate("Муромский", "Игнатий", "Львович", Sex.MALE, 39
                 , 5, 2));
 
         System.out.println("\n\nTreeSet - отсортировали по релевантности и рейтингу\n");
@@ -54,7 +54,7 @@ public class Main {
 
     // вариант с List
     //  ... тут потребуется описать класс, реализующий Comparator
-    public static void tryList(TreeSet<Candidate> setCandidates){
+    public static void tryList(TreeSet<Candidate> setCandidates) {
 
         List<Candidate> candidates = new ArrayList<>();
         NavigableSet<Candidate> newSet = setCandidates.descendingSet();
--- 3.java_oop/lesson.8.treemap_treeset/candidate/src/ru/maki/Sex.java
+++ 3.java_oop/lesson.8.treemap_treeset/candidate/src/ru/maki/Sex.java
@@ -1,4 +1,5 @@
 package ru.maki;
 
-public enum Sex { MALE, FEMALE
+public enum Sex {
+    MALE, FEMALE
 }
