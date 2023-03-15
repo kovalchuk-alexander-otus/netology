49eaf67717f954177fb7775e5af9ca2ade75eaa8
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-03-15 04:26:18 +0300

    main: домашка по модулю Java ООП : Queue

---
+++ 3.java_oop/lesson.9.queue/salon/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> firstClients = List.of("Anya", "Sveta", "Olya", "Alexandra", "Ruslana", "Olesya", "Vika");
+
+        Queue<String> queue = new ArrayDeque<>(firstClients);
+
+        while (!queue.isEmpty()) {
+
+            if (Math.random() < 0.5) { // проверка условия, которое срабатывает с 50% вероятностью
+                queue.offer("a friend of " + queue.peek());
+            }
+            System.out.printf("%s  сделала новый маникюр%n", queue.poll());
+        }
+    }
+}
