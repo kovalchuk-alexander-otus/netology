ebf79abe2199d28695e56a917c32a233a2dd3d33
alexander.kovalchuk <alexander.kovalchuk.aka.maki@gmail.com>
2023-03-20 01:26:58 +0300

    main: домашка по java-core : модульный проект, сборка на maven/gradle

---
+++ 4.java_core/lesson.4.maven.gradle/gradle-module/db/src/main/java/DbSetting.java
@@ -0,0 +1,10 @@
+public class DbSetting {
+
+    private final String name;
+    private final String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
