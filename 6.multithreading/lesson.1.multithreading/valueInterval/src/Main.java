743dc404915b7faf96a1b6b199589406e36a3f48
kovalchuk.alexander <kovalchuk.alexander.aka.maki@gmail.com>
2023-05-11 15:28:39 +0300

    многопоточка - вторая домашка (решение)

--- 6.multithreading/lesson.1.multithreading/valueInterval/src/Main.java
+++ 6.multithreading/lesson.1.multithreading/valueInterval/src/Main.java
@@ -1,18 +1,24 @@
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException, ExecutionException {
     String[] texts = new String[25];
 
-    List<Thread> threads = new ArrayList<>(); // объявляем массив потоков
+    final ExecutorService threadPool = Executors.newFixedThreadPool(25);
+    List<Future> futures = new ArrayList<>(); // объявляем массив потоков
 
     for (int i = 0; i < texts.length; i++) {
       texts[i] = generateText("aab", 30_000);
 
       String text = texts[i]; // сохраним строчку в переменную
 
-      Runnable runnable = () -> { // оформим лямбду (логику не менял)
+      Callable<Integer> callable = () -> { // оформим лямбду (логику не менял)
         int maxSize = 0;
         for (int s = 0; s < text.length(); s++) {
           for (int j = 0; j < text.length(); j++) {
@@ -32,24 +38,25 @@ public class Main {
           }
         }
         System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        return maxSize;
       };
 
-      threads.add(new Thread(runnable)); // добавим очередной поток с нашей лямбдой
+      futures.add(threadPool.submit(callable)); // добавим очередной поток с нашей лямбдой
     }
 
     long startTs = System.currentTimeMillis(); // start time
 
-    for (Thread thread : threads) { // блок-запуска выполнения всех потоков
-      thread.start();
+    int maxRange = 0;
+    for (Future<Integer> future : futures) { // блок-запуска выполнения всех потоков
+      maxRange = Math.max(maxRange, future.get());
     }
-
-    for (Thread thread : threads) { // блок-ожидания завершения выполнения всех потоков
-      thread.join();
-    }
+    System.out.printf("Максимальный интервал значений: %d%n%n", maxRange);
 
     long endTs = System.currentTimeMillis(); // end time
 
     System.out.println("Time: " + (endTs - startTs) + "ms");
+
+    threadPool.shutdown();
   }
 
   public static String generateText(String letters, int length) {
