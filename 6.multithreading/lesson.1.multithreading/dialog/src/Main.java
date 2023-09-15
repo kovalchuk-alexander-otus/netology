05e009f64dbe037a233a92eeb446088b2a33e2fe
Alex Kovach <alexander.kovalchuk.aka.maki@gmail.com>
2023-09-15 03:04:40 +0300

    ДЗ #1

---
+++ 6.multithreading/lesson.1.multithreading/dialog/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static void dialog(int thread) {
+        while (true) {
+            System.out.printf("Я поток %s. Всем привет!", thread);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Создаю потоки...");
+
+        List<Thread> threads = IntStream.range(1, 5).
+                mapToObj((v) -> {
+                    Runnable dialog = () -> dialog(v);
+                    Thread thread = new Thread(dialog);
+                    thread.setName("поток " + v);
+                    thread.start();
+                    return thread;
+                }).collect(Collectors.toList());
+
+        Thread.sleep(15000);
+        System.out.println("Завершаю все потоки.");
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+
+    }
+}
