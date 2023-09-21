b32d8be86ee859db2b6b5e3d3f4b615b59933997
Alex Kovach <alexander.kovalchuk.aka.maki@gmail.com>
2023-09-21 21:19:51 +0300

    add implemented

--- 6.multithreading/lesson.5.tdd_ddd/phoneBook/src/main/java/ru/maki/PhoneBook.java
+++ 6.multithreading/lesson.5.tdd_ddd/phoneBook/src/main/java/ru/maki/PhoneBook.java
@@ -2,7 +2,9 @@ package ru.maki;
 
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,8 +14,8 @@ import java.util.Map;
 @NoArgsConstructor
 public class PhoneBook {
 
-    Map<String, String> phoneBook = new HashMap<>();
-    Map<String, String> nameBook = new HashMap<>();
+    private Map<String, String> nameBook = new HashMap<>();
+    private Map<String, List<String>> phoneBook = new HashMap<>();
 
     /**
      * Добавление в Телефонную книгу имени с номером
@@ -22,6 +24,39 @@ public class PhoneBook {
      * @return количество контактов после добавления
      */
     public int add(String name, String phone) {
-        return 0;
+
+        // добавление в список имен
+        this.nameBook.put(name, phone);
+
+        // добавление в список номеров
+        String link = null;
+        String key = null;
+        List<String> value = null;
+        for (Map.Entry<String, List<String>> entry : this.phoneBook.entrySet()) {
+            for (String person : entry.getValue()) {
+                if (person.equals(name)) {
+                    link = person; // ссылка непосредственно на имя в списке
+                    key = entry.getKey(); // телефон
+                    value = entry.getValue(); // список пользователей с таким номером
+                    break;
+                }
+            }
+            if (key != null) break;
+        }
+
+        if (key != null) { // контакт с данным именем ранее уже был в Телефонной книге (имя следует удалить)
+            if (value.size() == 1) {
+                this.phoneBook.remove(key);
+            } else {
+                this.phoneBook.get(key).remove(link);
+            }
+        }
+
+        if (!this.phoneBook.containsKey(phone)) {
+            this.phoneBook.put(phone, new ArrayList<>());
+        }
+        this.phoneBook.get(phone).add(name);
+
+        return this.nameBook.size();
     }
 }
--- 6.multithreading/lesson.5.tdd_ddd/phoneBook/src/test/java/ru/maki/PhoneBookTest.java
+++ 6.multithreading/lesson.5.tdd_ddd/phoneBook/src/test/java/ru/maki/PhoneBookTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.*;
 
 
 public class PhoneBookTest {
+    /**
+     * Простой тест - добавление 3х разных контактов в Телефонную книгу
+     */
     @Test
     public void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
@@ -17,6 +20,10 @@ public class PhoneBookTest {
         assertEquals(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), expected, actual);
         // assertTrue(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), actual == expected);
     }
+
+    /**
+     * Сложный тест - добавление в Телефонную книгу одного имени два раза
+     */
     @Test
     public void testAddWhitRepeatName() {
         PhoneBook phoneBook = new PhoneBook();
@@ -26,6 +33,22 @@ public class PhoneBookTest {
         phoneBook.add("Felix", "13442");
         actual = phoneBook.add("Junior", "13423");
         phoneBook.add("Alex", "23235");
+        assertEquals(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), expected, actual);
+        // assertTrue(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), actual == expected);
+    }
+
+    /**
+     * Сложный тест - добавление в Телефонную книгу одинаковых номеров по разным именам
+     */
+    @Test
+    public void testAddWhitRepeatPhone() {
+        PhoneBook phoneBook = new PhoneBook();
+        int expected = 4;
+        int actual;
+        phoneBook.add("Alex", "23553");
+        phoneBook.add("Felix", "23553");
+        phoneBook.add("Junior", "23553");
+        actual = phoneBook.add("Angie", "52354");
         assertEquals(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), expected, actual);
         // assertTrue(String.format("Число контактов [%s] не соответствует заведенному [%s]", expected, actual), actual == expected);
     }
